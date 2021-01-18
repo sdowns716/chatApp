8f88b35d0a6d3ae75ef19e980ee0d0fa4df10da8
sdowns716 <66967392+sdowns716@users.noreply.github.com>
2021-01-18 12:41:28 -0500

    5.4 add firestore

--- components/Chat.js
+++ components/Chat.js
@@ -2,92 +2,130 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+//require Firebase and Cloud Firestore
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
       messages: [],
+      user: {
+        _id: "",
+        name: "",
+      },
+      loggedInText: "",
+    };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYuTnoyae3TTpOF8eKHIuNTvFOxnI8jww",
+    authDomain: "chatapp-a61ac.firebaseapp.com",
+    projectId: "chatapp-a61ac",
+    storageBucket: "chatapp-a61ac.appspot.com",
+    messagingSenderId: "544735696816",
+    appId: "1:544735696816:web:bc4588b29d5587d4d6a875",
+    measurementId: "G-PYM6WPVV34",
+  }
+
+  if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
     }
+
+  this.referenceMessages = firebase.firestore().collection("messages");
   }
 
+  //authenticates the user, sets the state to sned messages and gets past messages
   componentDidMount() {
-    // access name prop from Start Screen and set as title in Navigation bar
-    let { name } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name });
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: `Hello, ${this.props.route.params.name}. Welcome to the chat!`,
-          createdAt: new Date(),
-          system: true,
-        },
-        {
-          _id: 2,
-          text: "This is a system message",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-      ],
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      //update user state with currently active user data
+      this.setState({
+        _id:  user.uid,
+        name: this.props.route.params.name,
+        loggedInText: 'Hello there',
+        messages: [],
+      });
     });
-  }
-  
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
-  }
+      this.unsubscribe = this.referenceMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    };
 
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
-      />
-    )
-  }
+  componentWillUnmount() {
+      this.authUnsubscribe();
+      this.unsubscribe();
+    }
+
+    onSend(messages = []) {
+      this.setState(
+        (previousState) => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }),
+        () => {
+          this.addMessages();
+        });
+    }
+
+    //Updates the messages in the state
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // loop through documents
+    querySnapshot.forEach((doc) => {
+      // get data snapshot
+      const data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  //Pushes messages to Firestore database
+  addMessages = () => {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: message.user,
+      sent: true,
+    });
+  };
 
   render() {
-    //access name and color props from StartScreen
-    let { name, color } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name, backgroundColor: color });
+    //Get seleceted background color
+    let bcolor = this.props.route.params.color;
+    //Get selected user name
+    let name = this.props.route.params.name;
+    //Set title to usernam
+    this.props.navigation.setOptions({ title: name });
 
-    return (
-      <View style={[styles.container, { backgroundColor: color }, {flex: 1}]}>
-        <Text style={styles.welcomeText}>Welcome to the Chat</Text>
-        <GiftedChat
-          renderBubble={this.renderBubble.bind(this)}
-          messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
-        />
-        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
-        }  
-      </View>
-      
-    );
-  }
+  return (
+    <View
+      style={{
+        flex: 1,
+        //Set background color to selected
+        backgroundColor: bcolor,
+      }}
+    >
+      <Text>{this.state.loggedInText}</Text>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={this.state.user}
+      />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  welcomeText: {
-    fontSize: 20,
-    color: "#D72BD8"
-  },
-});
+}
